e363928b927ade61e019a5ab194bd30e77fd98a4
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-06-14 18:29:40 +0600

    everything alright so far

--- src/pages/Dashboard/InstructorDashboard/Feedback.jsx
+++ src/pages/Dashboard/InstructorDashboard/Feedback.jsx
@@ -1,50 +1,21 @@
-// import { useQuery } from "@tanstack/react-query";
-// import useAxiosSecure from "../../../hooks/useAxiosSecure";
-// import useAuth from "../../../hooks/useAuth";
-
-// const Feedback = () => {
-//     const { user } = useAuth();
-//   const [axiosSecure] = useAxiosSecure();
-//   const { data: users = [], refetch } = useQuery(["classes"], async () => {
-//     const res = await axiosSecure.get("/classes");
-//     return res.data;
-//   });
-//   //   const paidClasses = users.filter(user => user.status === "paid")
-//   const userEmail = user.email;
-//   const paidClasses = users.filter((payment) => payment.email === userEmail);
-//   const feedbacks = paidClasses.filter((feedback) => feedback.feedback !== null);
- 
-  
-//     return (
-//         <div>
-//             <h2 className="text-5xl text-center my-10">No of Feedback from Admin: {feedbacks?.length}</h2>
-//             {feedbacks.map(paid => <li className="text-2xl ml-10" key={paid._id}>{paid.feedback}</li> )}
-//         </div>
-//     );
-// };
-
-// export default Feedback;
 
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import useAuth from "../../../hooks/useAuth";
+
 import { useParams } from "react-router-dom";
 
 const Feedback = () => {
   const {id} = useParams();
 
-  // const { user } = useAuth();
   const [axiosSecure] = useAxiosSecure();
   const { data: users = [], refetch } = useQuery(["classes"], async () => {
     const res = await axiosSecure.get("/classes");
     return res.data;
   });
-  // const userEmail = user.email;
+ 
   const paidClasses = users.filter((payment) => payment._id === id && payment.feedback !==null);
-  // const feedbacks = paidClasses.filter(
-  //   (feedback) => feedback.feedback !== null && feedback.id === id
-  // );
+
 
   return (
     <div>
