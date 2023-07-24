486463cd2e344eaa11fca8d1e8a4cde41816811a
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-07-24 20:06:03 +0200

    banner component responsivenes and image sizes fixed

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,70 +1,30 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import React from "react";
 import img1 from '../../../assets/home/01.jpg';
- import img2 from '../../../assets/home/02.jpg';
- import img3 from '../../../assets/home/03.png';
- import img4 from '../../../assets/home/04.jpg';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import img2 from '../../../assets/home/02.jpg';
+import img3 from '../../../assets/home/03.png';
+import img4 from '../../../assets/home/04.jpg';
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
- 
-
-
-// import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
-
-const Banner = () =>  {
-    return (
-        <>
-            <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <img
-                        className="object-fill w-full h-96 md:h-1/2"
-                        src={img4}
-                        alt="image slide 1"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        className="object-fill w-full h-96 md:h-1/2"
-                        src={img2}
-                        alt="image slide 2"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        className="object-fill w-full h-96 md:h-1/2"
-                        src={img3}
-                        alt="image slide 3"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        className="object-fill w-full h-96 md:h-1/2"
-                        src={img1}
-                        alt="image slide 3"
-                    />
-                </SwiperSlide>
-            </Swiper>
-        </>
-    );
+const Banner =()=> {
+  return (
+    <div className="">
+      <Carousel useKeyboardArrows={true} className="text-center">
+       
+          <div className="slide h-full md:h-[800px]">
+            <img className="h-full" alt="sample_file" src={img1} />
+          </div>
+          <div className="slide h-full md:h-[800px]">
+            <img className="h-full" alt="sample_file" src={img2} />
+          </div>
+          <div className="slide h-full md:h-[800px]">
+            <img className="h-full" alt="sample_file" src={img3} />
+          </div>
+          <div className="slide h-full md:h-[800px]">
+            <img className="h-full" alt="sample_file" src={img4} />
+          </div> 
+      </Carousel>
+    </div>
+  );
 }
 export default Banner;
