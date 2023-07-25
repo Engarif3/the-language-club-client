3dc1b0aa3f0688ded2b4858e65bb4f55ec379e0a
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-07-25 23:49:38 +0200

    All instructors card design fixed

--- src/pages/Instructors/Instructor.jsx
+++ src/pages/Instructors/Instructor.jsx
@@ -9,15 +9,15 @@ const Instructor = ({instructor}) => {
   return (
     
       <Container>
-        <div className="card  bg-base-100 shadow-xl">
+        <div className="card  bg-base-100 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:shadow-2xl duration-300">
           <figure>
             <img
-             className="h-80 w-11/12 rounded-lg cover m-4"
+             className="h-72 w-full rounded-t-lg object-cover"
              src={photo}
               alt="Instructor Image"
             />
           </figure>
-          <div className="card-body">
+          <div className="card-body md:h-[8rem] lg:h-[8rem] h-[10rem]">
             <h2 className="card-title">Name:{name}</h2>
             <p>Email: {email}</p>
           </div>
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -21,7 +21,7 @@ const Instructors = () => {
         <title>The Language Club | Instructors</title>
       </Helmet>
       <h2 className="text-5xl text-center py-12">All Instructors Information</h2>
-      <div className="grid md:grid-cols-3 gap-8 px-12">
+      <div className="grid md:grid-cols-3 gap-8 px-12 ">
         {instructors.map(instructor => (
           <Instructor key={instructor._id} instructor={instructor} />
         ))}
