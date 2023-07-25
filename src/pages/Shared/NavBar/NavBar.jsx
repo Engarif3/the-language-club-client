c84e082030a1993c926c1d81f0d2a9a4ae165b95
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-07-25 12:55:15 +0200

    navbar color changed

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
   };
   return (
     <Container>
-      <div className="navbar bg-teal-300">
+      <div className="navbar bg-indigo-50">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
