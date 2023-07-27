6a95bde95d25bbf9442ea06d3f30f5d3341770aa
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-07-27 14:00:19 +0200

    banner images rounded and got margin top

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,19 +8,19 @@ import img4 from '../../../assets/home/04.jpg';
 import img6 from '../../../assets/home/06.png';
 import Container from "../../../Container";
 
-const images =[img1, img2, img3, img4, img6]
+const images =[img4, img1, img3, img2, img6]
 
 const Banner =()=> {
   return (
-    <>
+    <div className="mt-4">
       <Carousel useKeyboardArrows={true} className="text-center">
           {images.map((URL, index) => (
-          <div className="slide h-full md:h-[800px]">
-            <img className="h-full" alt="banner_image" src={URL} key={index} />
+          <div className="slide h-full md:h-[800px] ">
+            <img className="h-full rounded-lg" alt="banner_image" src={URL} key={index} />
           </div>
         ))}
       </Carousel>
-    </>
+    </div>
   );
 }
 export default Banner;
