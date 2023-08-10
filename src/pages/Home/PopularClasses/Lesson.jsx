126728e5f280f58a065b756008ae1eafb29ed4fb
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-08-10 12:40:33 +0200

    dark mode added to home patge cards

--- src/pages/Home/PopularClasses/Lesson.jsx
+++ src/pages/Home/PopularClasses/Lesson.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import Container from '../../../Container';
+import useAuth from '../../../hooks/useAuth';
+
 
 
 const Lesson = ({ course }) => {
 
+  const {darkMode} = useAuth();
   const { classImage, nameClass, instructorName, email, seats, price } = course;
-  // console.log(instructorEmail)
+
 
   return (
     <div>
       
-        <div className="card bg-base-100 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300">
+        {/* <div className="card bg-base-100 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300"> */}
+        <div className={darkMode? "card bg-[#2D2D2D] text-white border-b-4 border-red-600 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300":"card bg-base-100 border-b-4 border-gray-600 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300"}>
           <figure>
             <img className="h-64 w-full rounded-t-lg object-fit" src={classImage} alt="Course Image" />
           </figure>
--- src/pages/Home/PopularInstructors/PopularInstructor.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructor.jsx
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 import {IoIosArrowDropright} from "react-icons/io";
 import InstructorModal from "../../../components/Modal/InstructorModal";
 import { Link, useNavigate} from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
 
 
 const PopularInstructor = ({item}) => {
-
+  const {darkMode} = useAuth();
   const navigate = useNavigate();
   const [modal,setModal] = useState(false);
   const [isOpen,setIsOpen] = useState(false);
@@ -18,7 +19,7 @@ const PopularInstructor = ({item}) => {
   return (
     <div>
        
-      <div className="card  bg-base-100 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300">
+      <div div className={darkMode? "card bg-[#2D2D2D] text-white border-b-4 border-red-600 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300":"card bg-base-100 border-b-4 border-gray-600 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-300"}>
         <figure>
           <img
             className="h-72 w-full rounded-t-lg object-fill"
